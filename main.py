fc7ddd471d3cc5c0a9301c38dd1574d18b93b8ac
luigoNugawi <148757377+luigoNugawi@users.noreply.github.com>
2023-10-24 10:18:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Que fish"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
